ae3c8d14895cc49e7c1be004bd9aa1b03f9f41e1
Uri <uri@uri.com>
2025-02-04 19:12:08 +0100

    Important Hotfix

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     };
     //changes for release
     // bug fixes
+    // HOTFIX!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
